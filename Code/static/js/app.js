176898ff68da74c75bdacfcdfc4534a8f3006ae8
Hocine Makhlouf <datamakhlouf@gmail.com>
2019-05-23 16:02:47 -0500

    hw14

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// reference the table
+var tbody = d3.select("tbody")
+
+// update the value of each cell with pplsReport values (date/time, city, state, country, shape, and comment)
+
+
+
+// Loop Through `data` and console.log each of the ppls Report   
+data.forEach(function(pplsReport){
+    console.log(pplsReport);
+    // append row for every sigle ppls report 
+    var row = tbody.append("tr");
+    //   console.log each value in ppls Report data by using  `Object.entries` 
+    Object.entries(pplsReport).forEach(function([key, value]){
+        console.log(key, value);
+        // Append a cell to the row for each value in pplsReport object 
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
