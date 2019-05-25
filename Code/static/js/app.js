f1d29542351b7162bec7f05fb9b6c6932ff5c0ab
Hocine Makhlouf <datamakhlouf@gmail.com>
2019-05-25 11:53:37 -0500

    FilteredData

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,33 +1,33 @@
 
-//1write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+// 1write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
 
 
 
 
-// // from data.js
-// //var tableData = data;
+// from data.js
+//var tableData = data;
 
-// // reference the table
-// var tbody = d3.select("tbody")
+// reference the table
+var tbody = d3.select("tbody")
 
-// // update the value of each cell with pplsReport values (date/time, city, state, country, shape, and comment)
+// update the value of each cell with pplsReport values (date/time, city, state, country, shape, and comment)
 
 
 
-// // Loop Through `data` and console.log each of the ppls Report   
-// data.forEach(function(pplsReport){
-//     console.log(pplsReport);
-//     // append row for every sigle ppls report 
-//     var row = tbody.append("tr");
-//     //   console.log each value in ppls Report data by using  `Object.entries` 
-//     Object.entries(pplsReport).forEach(function([key, value]){
-//         console.log(key, value);
-//         // Append a cell to the row for each value in pplsReport object 
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
+// Loop Through `data` and console.log each of the ppls Report   
+data.forEach(function(pplsReport){
+    console.log(pplsReport);
+    // append row for every sigle ppls report 
+    var row = tbody.append("tr");
+    //   console.log each value in ppls Report data by using  `Object.entries` 
+    Object.entries(pplsReport).forEach(function([key, value]){
+        console.log(key, value);
+        // Append a cell to the row for each value in pplsReport object 
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
 
 
 
@@ -41,59 +41,61 @@ var events = data;
 
 
 // Select the submit button
-var filter_btn = d3.select("#filter-btn");
+var submit = d3.select("#filter-btn");  //
 
-// filter_btn.on("click", function() {
+submit.on("click", function() {
 
   // Prevent the page from refreshing
-  // d3.events.preventDefault();
+  d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("text");
+  var inputValue = inputElement.property("value"); //**value */
 
   console.log(inputValue);
-  // console.log(events);
+  console.log(events);
   
-//   var filteredData = events.filter(events => events.datetime === inputValue);
+  var filteredData = events.filter(events => 
+    events.datetime === inputValue
+  );
 
-//   if (events.datetime === inputValue){
-//     console.log(filteredData);
-//   }
-
-    
-// });
-
-
-//// append filtered data to the same table 
-
-
-//var tableData = filteredData;
-
-// reference the table
-// var tbody = d3.select("tbody")
+  if (events.datetime === inputValue){
+    console.log(filteredData);
+  }
+     
+  console.log(filteredData);
 
 
 
-// // update the value of each cell with pplsReport values (date/time, city, state, country, shape, and comment)
+// append filtered data to the same table 
+
+
+// var tableData = filteredData;
+
+//reference the table
+var tbody = d3.select("tbody")
 
 
 
-// // Loop Through `data` and console.log each of the ppls Report   
-// filteredData.forEach(function(pplsReport){
-//     console.log(pplsReport);
-//     // append row for every sigle ppls report 
-//     var row = tbody.append("tr");
-//     //   console.log each value in ppls Report data by using  `Object.entries` 
-//     Object.entries(pplsReport).forEach(function([key, value]){
-//         console.log(key, value);
-//         // Append a cell to the row for each value in pplsReport object 
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
+// update the value of each cell with pplsReport values (date/time, city, state, country, shape, and comment)
 
 
 
+// Loop Through `data` and console.log each of the ppls Report   
+filteredData.forEach(function(pplsReport){
+    console.log(pplsReport);
+    // append row for every sigle ppls report 
+    var row = tbody.append("tr");
+    //   console.log each value in ppls Report data by using  `Object.entries` 
+    Object.entries(pplsReport).forEach(function([key, value]){
+        console.log(key, value);
+        // Append a cell to the row for each value in pplsReport object 
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+});
